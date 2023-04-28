46f9c518876bd5c773b7321c0f75158809bfed07
Catherine Nevin <cat_nevin@hotmail.com>
2023-04-28 10:48:20 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// Set up your dependencies!
+// Require necessary NPM packages
+const express = require("express")
+const mongoose = require("mongoose")
+
+// Initialise the express application:
+const app = express()
+
+// You don't really want to hard code your port!
+// Define the PORT for the API to run on
+const port = process.env.PORT || 5000 
+        // if the port can't be found in teh environment, 
+        // 5000 is the fall back port
+
+// Start the server and listen for requests on the given port
+app.listen(port, () => console.log(`blogy is listening on port ${port}`))
+    // logging is imporant when you are deploying to a server 
+    // so you know what is going on. They are the only way you
+    // can know 
+
